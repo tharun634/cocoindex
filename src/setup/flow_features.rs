cdc3db5ba460020b19c084fbbe93d2d894a7025e
George <hejiangzhou@gmail.com>
2025-08-15 13:08:15 -0700

    chore: don't turn on source states table by default, for now (#871)

--- src/setup/flow_features.rs
+++ src/setup/flow_features.rs
@@ -3,5 +3,5 @@ use crate::prelude::*;
 pub const SOURCE_STATE_TABLE: &str = "source_state_table";
 
 pub fn default_features() -> BTreeSet<String> {
-    vec![SOURCE_STATE_TABLE.to_string()].into_iter().collect()
+    BTreeSet::new()
 }
